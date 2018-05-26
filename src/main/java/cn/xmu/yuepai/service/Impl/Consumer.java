6681df6d20b7327dc8d41bb35ed316bbe2990067
WongGamTou <286963982@qq.com>
2018-05-26 18:16:24 +0800

    modify activemq

--- src/main/java/cn/xmu/yuepai/service/Impl/Consumer.java
+++ src/main/java/cn/xmu/yuepai/service/Impl/Consumer.java
@@ -3,12 +3,12 @@ package cn.xmu.yuepai.service.Impl;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Consumer {
 
     // 使用JmsListener配置消费者监听的队列，其中text是接收到的消息
-    @JmsListener(destination = "zh-topic")
-    public void receiveQueue(String text) {
-        System.out.println("Consumer receive:"+text);
-    }
+//    @JmsListener(destination = "zh-topic")
+//    public void receiveQueue(String text) {
+//        System.out.println("Consumer receive:"+text);
+//    }
 }
--- src/main/java/cn/xmu/yuepai/view/ActiveMqController.java
+++ src/main/java/cn/xmu/yuepai/view/ActiveMqController.java
@@ -1,28 +1,153 @@
 package cn.xmu.yuepai.view;
 
 import cn.xmu.yuepai.service.ProductService;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.Topic;
+import javax.jms.*;
+import javax.naming.NamingException;
 
 @RestController
 public class ActiveMqController {
-    @Autowired
-    private Topic topic;
+
+    //默认连接用户名
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
+    //默认连接密码
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+    //默认连接地址
+    private static final String BROKEURL = "tcp://localhost:61616";
+
+//    @Autowired
+//    private Topic topic;
 
     @Autowired
     private ConnectionFactory connectionFactory;
 
-    @Autowired
-    private ProductService productService;
+//    @Autowired
+//    private ProductService productService;
 
     @RequestMapping("/topic/{msg}")
-    public void sendTopic(@PathVariable("msg") String msg){
-        System.out.println("Producer send.");
-        productService.sendMessage(this.topic, msg);
+    public void sendTopic(@PathVariable("msg") String msg) {
+        Connection connection = null;//连接
+
+        Session session;//会话 接受或者发送消息的线程
+
+        Destination destination;//消息的目的地
+
+        //消息生产者
+        MessageProducer messageProducer;
+
+        //实例化连接工厂(连接到ActiveMQ服务器)
+        connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKEURL);
+        try{
+            //通过连接工厂获取连接
+            connection = connectionFactory.createConnection();
+            //创建session
+            session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+            //创建一个名称为zh-topic的消息队列(生产者生成的消息放在哪)
+            //String d = "topic-" + msg;
+            destination = session.createTopic("zh-topic");
+            //创建消息生产者
+            messageProducer = session.createProducer(destination);
+            messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);
+            //启动连接
+            connection.start();
+            //发送消息
+            sendMessage(session, messageProducer, msg);
+
+            session.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+//        System.out.println("Producer send.");
+//        productService.sendMessage(this.topic, msg);
+    }
+
+    @RequestMapping("/topic/receive")
+    public void receiveTopic() throws NamingException {
+        Connection connection = null;//连接
+
+        Session session;//会话 接受或者发送消息的线程
+
+        Destination destination;//消息的目的地
+
+        MessageConsumer messageConsumer;//消息的消费者
+
+        Topic topic;//消息的目的地
+
+        try {
+            //for (int i=1; i<=2; i++) {
+                System.out.println("建立连接！");
+                //实例化连接工厂(连接到ActiveMQ服务器)
+                connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD, BROKEURL);
+                //System.out.println(1);
+                //通过连接工厂获取连接
+                connection = connectionFactory.createConnection();
+                //System.out.println(2);
+                connection.setClientID("winner_0715");
+                //System.out.println(3);
+                //创建session
+                session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+                //System.out.println(4);
+
+                //生产者将消息发送到zh-topic，所以消费者要到zh-topic去取
+                //String to = "topic-" + i;
+                topic = session.createTopic("zh-topic");
+                //System.out.println(5);
+                //创建消息消费者
+                TopicSubscriber consumer = session.createDurableSubscriber(topic, "t1");
+                //System.out.println(6);
+
+                //启动连接
+                connection.start();
+                //System.out.println(7);
+
+                Message message = consumer.receive();
+                //System.out.println(8);
+                while (message != null) {
+                    TextMessage txtMsg = (TextMessage) message;
+                    System.out.println("收到消息：" + txtMsg.getText());
+                    //没这句有错
+                    message = consumer.receive(5000);
+                    //System.out.println(9);
+                }
+                session.commit();
+                session.close();
+                connection.close();
+            //}
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 发送消息
+     *
+     * @param session
+     * @param messageProducer 消息生产者
+     * @throws Exception
+     */
+    public static void sendMessage(Session session, MessageProducer messageProducer, String msg) throws Exception {
+            //创建一条文本消息
+            TextMessage message = session.createTextMessage("ActiveMQ 发送消息" + msg);
+            System.out.println("发送消息：Activemq 发送消息" + msg);
+            //通过消息生产者发出消息
+            messageProducer.send(message);
     }
 }
