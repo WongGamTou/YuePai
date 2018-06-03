cb55f0ffebb9eb38492200dcd04d0b170167ca82
zjz <24320152202854@stu.xmu.edu.cn>
2018-06-03 20:22:14 +0800

    share.js

---
+++ src/main/resources/static/js/share.js
@@ -0,0 +1,69 @@
+// ------- support functions ---------------
+function jumpphoto(cid) {
+    window.location.href = '/' + cid + '/photo';
+}
+
+function jumpshare(cid) {
+    window.location.href = '/' + cid + '/share';
+}
+
+function jumpprofile(cid) {
+    window.location.href = '/' + cid + '/profile';
+}
+
+// -------- photo page ---------------
+
+function followphotolist(){
+    $.ajax({
+        type:'get',
+        url:'/' + localStorage.getItem("userId") + '/imageshare/attention',
+        dataType: "json",
+        contentType: "application/json;",
+        success: function (invitation, textStatus, xhr) {
+            if(xhr.status == 200) {
+                var content = document.getElementById("getfollow");
+                var str = "";
+                for(var i=0; i<data.length; i++) {
+                    str +=         "                                        <div class=\"row\">\n"+
+                                   "                                            <div class=\"col-sm-6 col-md-4\">\n"+
+                                   "                                                <div class=\"thumbnail\">\n"+
+                                   "                                                    <img data-toggle=\"modal\" data-target=\"#myModal\" src=\"../../static/img/img1.png\" alt=\"...\">\n"+
+                                   "                                                    <div class=\"caption\">\n"+
+                                   "                                                        <h3>匪我思存</h3>\n"+
+                                   "                                                        <p>一只可爱小猫咪的艺术照哈哈哈</p>\n"+
+                                   "                                                        <p class=\"card-icon like\">&#10084;</p>\n"+
+                                   "                                                    </div>\n"+
+                                   "                                                </div>\n"+
+                                   "                                            </div>\n"+
+                                   "                                            <div class=\"col-sm-6 col-md-4\">\n"+
+                                   "                                                <div class=\"thumbnail\">\n"+
+                                   "                                                    <img data-toggle=\"modal\" data-target=\"#myModal\" src=\"../../static/img/img2.png\" alt=\"...\">\n"+
+                                   "                                                    <div class=\"caption\">\n"+
+                                   "                                                        <h3>匪我思存</h3>\n"+
+                                   "                                                        <p>一只可爱小猫咪的艺术照哈哈哈</p>\n"+
+                                   "                                                        <p class=\"card-icon like\">&#10084;</p>\n"+
+                                   "                                                    </div>\n"+
+                                   "                                                </div>\n"+
+                                   "                                            </div>\n"+
+                                   "                                            <div class=\"col-sm-6 col-md-4\">\n"+
+                                   "                                                <div class=\"thumbnail\">\n"+
+                                   "                                                    <img data-toggle=\"modal\" data-target=\"#myModal\" src=\"../../static/img/img3.png\" alt=\"...\">\n"+
+                                   "                                                    <div class=\"caption\">\n"+
+                                   "                                                        <h3>匪我思存</h3>\n"+
+                                   "                                                        <p>一只可爱小猫咪的艺术照哈哈哈</p>\n"+
+                                   "                                                        <p class=\"card-icon like\">&#10084;</p>\n"+
+                                   "                                                    </div>\n"+
+                                   "                                                </div>\n"+
+                                   "                                            </div>\n"+
+                                   "                                        </div>";
+                }
+                content.innerHTML = str;
+            }
+        },
+        statusCode:{
+            401: function (){//statuscode unknown
+                alert("获取信息失败");
+            }
+        }
+    });
+}
